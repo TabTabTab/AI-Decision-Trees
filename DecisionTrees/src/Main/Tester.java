512050fc1676fa1e4d18e1f3864e38c4a91b34a3
Johan Nyholm <hansjohannyholm@gmail.com>
2015-03-05 02:45:15 +0100

    Revert "User can now choose which classifier they want"
    This reverts commit a3fe3693a4b727febdf87e8bbed6f8c866a05298.

--- DecisionTrees/src/Main/Tester.java
+++ DecisionTrees/src/Main/Tester.java
@@ -2,9 +2,6 @@ package Main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
 
 import Parser.WekaParser;
 import Structures.Attribute;
@@ -18,9 +15,7 @@ public class Tester {
 		File file=new File("TrainingSet.ARFF");
 		try {
 			TrainingData trainingData=wParser.parseFile(file);
-			
-			//Attribute classifier=trainingData.getAllAttributes().get(trainingData.getAllAttributes().size()-1);
-			Attribute classifier=getUserChosenClassifier(trainingData.getAllAttributes());
+			Attribute classifier=trainingData.getAllAttributes().get(trainingData.getAllAttributes().size()-1);
 			TreeMaker maker=new TreeMaker(classifier);
 			Node tree=maker.makeTree(trainingData);
 			System.out.println(tree);
@@ -28,26 +23,5 @@ public class Tester {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
-	public static Attribute getUserChosenClassifier(ArrayList<Attribute> attributes) {
-	    String[] choices = new String[attributes.size()];//{ "A", "B", "C", "D", "E", "F" };
-	    ArrayList<String> indexLookup=new ArrayList<>();
-	    for(int i=0;i<choices.length;i++){
-	    	choices[i]=attributes.get(i).getName();
-	    	indexLookup.add(choices[i]);
-	    }
-	    String attributeName = (String) JOptionPane.showInputDialog(null, "Classifier",
-	        "Pick your desired classifier", JOptionPane.QUESTION_MESSAGE, null, // Use
-	                                                                        // default
-	                                                                        // icon
-	        choices, // Array of choices
-	        choices[0]); // Initial choice
-	    
-	    if(attributeName==null){
-	    	return getUserChosenClassifier(attributes);
-	    }
-	    int index=indexLookup.indexOf(attributeName);
-	    Attribute chosenAttribute=attributes.get(index);
-	    return chosenAttribute;
 	}
 }
